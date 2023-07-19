4e56cb936f4753d5415622b9cb3edd3e7403ae63
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-07-19 17:35:51 -0300

    Estiliza a página de perfil

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,27 +7,23 @@ const PLACEHOLDER = `https://st3.depositphotos.com
 
 function Profile({ username, image, email, decks }) {
   return (
-    <>
-      <header>
-        <div>
-          <img src={ image || PLACEHOLDER } width={ 200 } height={ 200 } />
-          <div>
-            <p>{ username }</p>
-            <p>{ email }</p>
-          </div>
-          <nav>
-            <ul>
-              <li><Link href="/">Decks Page</Link></li>
-              <li><Link href="/">Cards Page</Link></li>
-              <li><Link href="/">Users Page</Link></li>
-            </ul>
-          </nav>
-        </div>
+    <div id="profile-container">
+      <header className="profile-header">
+        <img src={ image || PLACEHOLDER } width={ 200 } height={ 200 } />
+        <p>{ username }</p>
+        <p>{ email }</p>
+        <nav>
+          <ul>
+            <li><Link href="/">Decks Page</Link></li>
+            <li><Link href="/">Cards Page</Link></li>
+            <li><Link href="/">Users Page</Link></li>
+          </ul>
+        </nav>
       </header>
-      <main>
+      <main className="profile-decks-container">
         <ProfileDecks decks={ decks }  />
       </main>
-    </>
+    </div>
   );
 }
 
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import '../styles/Profile.css';
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
